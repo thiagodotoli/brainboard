610e674d75b3255c7eb8e3043080a6dec46a18b2
thiagodotoli <thiago.dotoli@rethink.dev>
2023-03-02 17:38:32 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,11 @@ resource "aws_lambda_function" "lambda_function_1" {
   tags = merge(var.tags, {})
 }
 
+resource "aws_lambda_function" "lambda_function_2" {
+  tags = merge(var.tags, {})
+}
+
+
 resource "aws_api_gateway_resource" "api_gateway_resource_2" {
 }
 
