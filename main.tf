3eeef05b498ba160945f27d2fe28d4e7ac15c257
Brainboard <no-reply@brainboard.co>
2023-03-03 17:57:07 +0000

    Brainboard update

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ resource "aws_lambda_function" "lambda_function_1" {
   tags = merge(var.tags, {})
 }
 
-resource "aws_api_gateway_resource" "api_gateway_resource_2" {
+resource "aws_elastic_beanstalk_environment" "elastic_beanstalk_environment_4" {
+  tags = merge(var.tags, {})
 }
 
